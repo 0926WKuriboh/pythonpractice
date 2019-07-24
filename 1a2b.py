c80275ce08ad6e0966a6ef6a136ad5d43fbeafe0
呂卓翰 <ryotaku@lvzhuohande-MacBook-Pro.local>
2019-07-24 12:37:25 +0800

    create 1a2b

---
+++ 1a2b.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[55]:
+
+
+import random 
+list = [n for n in range(10)]
+answer = random.sample(list,4)
+print(answer)
+
+def abab(string) :
+    a=0
+    b=0    
+    for n in range(4):
+        if int(string[n]) == answer[n]:
+            a+=1
+        elif int(string[n]) in answer :
+            b+=1
+    print(a,"a",b,"b")
+    return a 
+
+xxx=input("請輸入數字：")
+guess = str(xxx) 
+while(abab(guess) != 4):
+    xxx=input("請輸入數字：")
+    guess = str(xxx) 
+print("恭喜！！")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
