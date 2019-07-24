7043e429f6f304ae6b2427b4e38bb1b65f690610
呂卓翰 <ryotaku@lvzhuohande-MacBook-Pro.local>
2019-07-24 13:25:05 +0800

    Update 1a2b.py

--- 1a2b.py
+++ 1a2b.py
@@ -1,24 +1,8 @@
-def abab(string) :
-    a=0
-    b=0
-    for n in range(4):
-        if int(string[n]) == answer[n]:
-            a+=1
-        elif int(string[n]) in answer :
-            b+=1
-    print(a,"a",b,"b")
-    return a #!/usr/bin/env python
-# coding: utf-8
-
-# In[55]:
-
-
 import random 
 list = [n for n in range(10)]
 answer = random.sample(list,4)
 print(answer)
-
-def abab(string) :
+def  abab(string) :
     a=0
     b=0    
     for n in range(4):
@@ -28,6 +12,7 @@ def abab(string) :
             b+=1
     print(a,"a",b,"b")
     return a 
+
 
 xxx=input("請輸入數字：")
 guess = str(xxx) 
